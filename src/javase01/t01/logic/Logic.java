877f053b6287d5c0b4eafd0e58172c5d7a609cd0
Maxim Levin <maximmlevin@gmail.com>
2017-04-24 02:45:46 +0300

    5/6

---
+++ src/javase01/t01/logic/Logic.java
@@ -0,0 +1,11 @@
+package javase01.t01.logic;
+
+/**
+ * Created by m-levin on 24.04.2017.
+ */
+public class Logic {
+
+    public String method(){
+        return "I am string in Logic.";
+    }
+}
---
+++ src/javase01/t01/main/Main.java
@@ -0,0 +1,16 @@
+package javase01.t01.main;
+
+/**
+ * Created by m-levin on 24.04.2017.
+ */
+
+import javase01.t01.logic.Logic;
+
+    public class Main {
+
+        public static void main(String[] args) {
+            Logic logic = new Logic();
+            System.out.println(logic.method());
+        }
+    }
+
