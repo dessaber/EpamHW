75477964ec14329290f5492b2b1346a60f4f7220
Maxim Levin <maximmlevin@gmail.com>
2017-04-24 03:10:35 +0300

    New commit

---
+++ src/javase01/t01/Compile.bat
@@ -0,0 +1,5 @@
+@echo off
+javac -d ..\..\..\out\production\HW\javase01\t01\logic logic\Logic.java
+javac -d ..\..\..\out\production\HW\javase01\t01\main main\Main.java
+java ..\..\..\out\production\HW\javase01\t01\main Main.class
+pause
