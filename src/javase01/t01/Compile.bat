e1019131d982010bd52becd7dba13fac6d95e8b8
Maxim Levin <maximmlevin@gmail.com>
2017-04-24 15:43:27 +0300

    New 5/6 commit'

--- src/javase01/t01/Compile.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-javac -d ..\..\..\out\production\HW\javase01\t01\logic logic\Logic.java
-javac -d ..\..\..\out\production\HW\javase01\t01\main main\Main.java
-java ..\..\..\out\production\HW\javase01\t01\main Main.class
-pause
--- src/javase01/t01/max/FindMax.java
+++ src/javase01/t01/max/FindMax.java
@@ -34,7 +34,7 @@ public class FindMax {
                 if (arrayLength > 0)
                     break;
             }
-            doubles = new double[2*arrayLength];
+            doubles = new double[arrayLength];
             for (int i = 0; i < doubles.length; ++i) {
                 doubles[i] = Double.parseDouble(reader.readLine());
             }
@@ -46,7 +46,7 @@ public class FindMax {
         return doubles;
     }
 
-    /** On every iteration this method counts symmetric sum of two elements of the array and outputs the maximum sum.
+    /** On every iteration this method counts sum of two elements of the array and outputs the maximum sum.
      *  If the array contains only one number, it will be the output.
      *
      * @param doubles array of real numbers
@@ -57,16 +57,21 @@ public class FindMax {
         double currentMax;
         if (doubles.length == 0) {
             System.out.println("Nothing to count");
-            return;
+        } else if (doubles.length == 1) {
+            System.out.println("The answer is " + doubles[0]);
+        } else if (doubles.length == 2) {
+            System.out.println("The answer is " + (doubles[0] + doubles[1]));
         } else {
-            currentMax = doubles[0] + doubles[doubles.length - 1];
-            double tempVariable;
-            for (int i = 1; i < doubles.length/2; i++) {
-                tempVariable = doubles[i] + doubles[doubles.length - 1 - i];
-                if (tempVariable > currentMax) {
-                    currentMax = tempVariable;
+            currentMax = doubles[0] + doubles[1];
+            double tempMax = currentMax;
+            for (int i = 2; i < doubles.length; ++i) {
+                if (i % 2 == 0) {
+                    currentMax = currentMax < tempMax ? tempMax : currentMax;
+                    tempMax = 0;
                 }
+                tempMax += doubles[i];
             }
+            currentMax = currentMax < tempMax ? tempMax : currentMax;
             System.out.println("The answer is " + currentMax);
         }
     }
